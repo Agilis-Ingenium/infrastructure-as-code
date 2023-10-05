76c20873a9534ac3936a46bcf74fac596ba1d8dd
Warren Byron <20091892@mail.wit.ie>
2023-10-05 20:25:16 +0100

    Create s3.tf
    Create a bucket for static hosting websites

---
+++ aws-infra/s3.tf
@@ -0,0 +1,37 @@
+resource aws_s3_bucket bucket {
+  bucket        = "www.nubadoo.com"
+  force_destroy = true
+}
+
+resource aws_s3_bucket_website_configuration website_configuration {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource aws_s3_bucket_acl bucket_acl {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
+}
+
+data aws_iam_policy_document policy {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.bucket.arn}/*"]
+  }
+}
+
+resource aws_s3_bucket_policy bucket_policy {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.policy.json
+}
