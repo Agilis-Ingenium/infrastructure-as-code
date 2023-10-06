55ebc9c43bc00f155dba314a59e8ebf88e694a97
Warren Byron <20091892@mail.wit.ie>
2023-10-06 20:42:17 +0100

    Update route53.tf

--- aws-infra/route53.tf
+++ aws-infra/route53.tf
@@ -2,19 +2,10 @@ data aws_route53_zone zone {
   name = var.domainName
 }
 
-resource aws_route53_record zone {
+resource aws_route53_record domain {
   zone_id = data.aws_route53_zone.zone.zone_id
   name    = format("%s.%s", var.subDomain, var.domainName)
   type    = "A"
-  ttl     = 300
-  records = [aws_s3_bucket.static-website.website_endpoint]
-}
-
-resource aws_route53_record domain {
-  zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "bucket"
-  type    = "A"
-
   alias {
     name    = resource.aws_s3_bucket.static-website.website_domain
     zone_id = resource.aws_s3_bucket.static-website.hosted_zone_id
