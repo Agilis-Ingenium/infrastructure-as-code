2fe6945cd1e61aae4e8615a90a3ae3a4846313b0
Warren Byron <20091892@mail.wit.ie>
2023-10-06 21:00:23 +0100

    Update s3.tf

--- aws-infra/s3.tf
+++ aws-infra/s3.tf
@@ -14,7 +14,28 @@ resource aws_s3_bucket_website_configuration static-website {
   }
 }
 
-resource aws_s3_bucket_policy static-website {
+resource aws_s3_bucket_ownership_controls static-website {
   bucket = aws_s3_bucket.static-website.id
-  policy = templatefile("s3-policy.json", { bucket = format("%s.%s", var.subDomain, var.domainName) })
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource aws_s3_bucket_public_access_block static-website {
+  bucket = aws_s3_bucket.static-website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource aws_s3_bucket_acl static-website {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.static-website,
+    aws_s3_bucket_public_access_block.static-website,
+  ]
+
+  bucket = aws_s3_bucket.static-website.id
+  acl    = "public-read"
 }
