e9891e2fe148105a3d4f4f1feba535adc0b3f642
Warren Byron <20091892@mail.wit.ie>
2023-10-06 19:16:25 +0100

    Update s3.tf

--- aws-infra/s3.tf
+++ aws-infra/s3.tf
@@ -1,10 +1,9 @@
-resource aws_s3_bucket bucket {
+resource aws_s3_bucket static-website {
   bucket        = "www.${var.apex_domain}"
-  force_destroy = true
 }
 
-resource aws_s3_bucket_website_configuration website_configuration {
-  bucket = aws_s3_bucket.bucket.id
+resource aws_s3_bucket_website_configuration static-website {
+  bucket = aws_s3_bucket.static-website.id
 
   index_document {
     suffix = "index.html"
@@ -15,23 +14,28 @@ resource aws_s3_bucket_website_configuration website_configuration {
   }
 }
 
-resource aws_s3_bucket_acl bucket_acl {
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "public-read"
-}
-
-data aws_iam_policy_document policy {
-  statement {
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.bucket.arn}/*"]
+resource aws_s3_bucket_ownership_controls static-website {
+  bucket = aws_s3_bucket.static-website.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource aws_s3_bucket_policy bucket_policy {
-  bucket = aws_s3_bucket.bucket.id
-  policy = data.aws_iam_policy_document.policy.json
+resource aws_s3_bucket_public_access_block static-website {
+  bucket = aws_s3_bucket.static-website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource aws_s3_bucket_acl my-static-website" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.static-website,
+    aws_s3_bucket_public_access_block.static-website,
+  ]
+
+  bucket = aws_s3_bucket.static-website.id
+  acl    = "public-read"
 }
