c46df5a5363b828c0736cec9db5bdb64c94f0418
Warren Byron <20091892@mail.wit.ie>
2023-10-06 15:23:19 +0100

    Create database on Elephant SQL

--- elephant_sql_infra/main.tf
+++ elephant_sql_infra/main.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_providers {
+    elephantsql = {
+      source = "elephantsql/elephantsql"
+    }
+  }
+}
 
+resource "elephantsql_instance" "sql_turtle" {
+  name   = "terraform-provider-test"
+  plan   = "turtle"
+  region = "amazon-web-services::eu-west-1"
+}
+
+output "psql_url" {
+  sensitive = true
+  value = elephantsql_instance.sql_turtle.url
+}
