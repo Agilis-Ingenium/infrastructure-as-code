63e9dcea6fb74a3a12de5689642eda0e69a10dea
Warren Byron <20091892@mail.wit.ie>
2023-10-06 20:13:36 +0100

    Update s3.tf

--- aws-infra/s3.tf
+++ aws-infra/s3.tf
@@ -1,5 +1,5 @@
 resource aws_s3_bucket static-website {
-  bucket        = "www.${var.domainName}"
+  bucket        = format("%s.%s", var.domainName, var.subDomain)
 }
 
 resource aws_s3_bucket_website_configuration static-website {
@@ -16,5 +16,5 @@ resource aws_s3_bucket_website_configuration static-website {
 
 resource aws_s3_bucket_policy static-website {
   bucket = aws_s3_bucket.static-website.id
-  policy = templatefile("s3-policy.json", { bucket = var.domainName })
+  policy = templatefile("s3-policy.json", { bucket = format("%s.%s", var.domainName, var.subDomain) })
 }
