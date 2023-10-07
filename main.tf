7e080f87e8f7925fa5203bbe1ad2fae4ea42309f
Warren Byron <wbyron@shoprite.co.za>
2023-10-07 16:03:57 +0100

    Reworked the static-website

--- main.tf
+++ main.tf
@@ -12,3 +12,10 @@ module elephant_sql {
     db_name  = var.db_name
     db_plan  = var.db_plan
 }
+
+module "static-website" {
+  source = "./static-website"
+  for_each = toset(var.sub_domains)
+  sub_domain_name = each.value
+  domain_name = var.domain
+}
