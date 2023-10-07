bbaaa6d5f6cd668ef942a355ee47e9be0b6750d0
Warren Byron <20091892@mail.wit.ie>
2023-10-07 14:15:32 +0100

    Update main.tf

--- aws-infra/main.tf
+++ aws-infra/main.tf
@@ -1,12 +1,3 @@
 provider aws {
   region  = var.region
 }
-
-terraform {
-  backend "s3" {
-    bucket         = "nubadoo-terraform-state-bucket"
-    key            = "terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "nubadoo-terraform-state"
-  }
-}
