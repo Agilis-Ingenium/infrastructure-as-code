3d81b60f0f57479710ff7a3775d8c4d5ab831d6a
Warren Byron <20091892@mail.wit.ie>
2023-10-07 14:18:08 +0100

    Put in terraform backend

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-for-nubadoo-com"
+    key            = "terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-state-for-nubadoo-com"
+  }
+}
+
 module elephant_sql {
     source   = "./elephant_sql"
     db_name  = "nubadoo"
