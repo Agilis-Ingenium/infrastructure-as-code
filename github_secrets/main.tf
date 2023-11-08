7b898f624f8bcd35fc8995e32701b1d83346f63a
Warren Byron <20091892@mail.wit.ie>
2023-11-08 17:43:06 +0000

    Update main.tf

--- github_secrets/main.tf
+++ github_secrets/main.tf
@@ -1,5 +1,11 @@
-resource "github_actions_secret" "example_secret" {
+resource "github_actions_secret" "psql_url" {
   repository       = "database"
-  secret_name      = "example_secret_name2"
+  secret_name      = "psql_url"
   plaintext_value  = var.psql_url
+}
+
+resource "github_actions_variable" "example_variable" {
+  repository       = "database"
+  variable_name    = "example_variable_name"
+  value            = var.psql_url
 }
