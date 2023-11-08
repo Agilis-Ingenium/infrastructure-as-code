3dc2da464950203aeb157bb316193b171bfdf18b
Warren Byron <20091892@mail.wit.ie>
2023-11-08 05:01:56 +0000

    Update function.tf

--- static-website/function.tf
+++ static-website/function.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudfront_function" "function" {
-  name    = "${var.sub_domain_name}.${var.domain_name}"
+  name    = "${var.sub_domain_name}"
   runtime = "cloudfront-js-1.0"
   publish = true
   code    = file("${path.module}/function-01.js")
