4dd7ad06041b74c998ba8cfaebbacdd85a715830
Warren Byron <20091892@mail.wit.ie>
2023-11-08 04:56:21 +0000

    Create function.tf

---
+++ static-website/function.tf
@@ -0,0 +1,6 @@
+resource "aws_cloudfront_function" "function" {
+  name    = "${var.sub_domain_name}.${var.domain_name}"
+  runtime = "cloudfront-js-1.0"
+  publish = true
+  code    = file("${path.module}/function-01.js")
+}
