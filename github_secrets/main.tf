f221fe95dc75d7223fa94349012fb03fc869eec5
Warren Byron <20091892@mail.wit.ie>
2023-11-08 11:41:19 +0000

    Update main.tf

--- github_secrets/main.tf
+++ github_secrets/main.tf
@@ -1,5 +1,5 @@
 resource "github_actions_secret" "example_secret" {
-  repository       = "Agilis-Ingenium/database"
+  repository       = "database"
   secret_name      = "example_secret_name"
   plaintext_value  = var.psql_url
 }
