c3028b279dda99befc13a646345a903549ed3b75
Warren Byron <20091892@mail.wit.ie>
2023-11-08 14:32:17 +0000

    Update provider.tf

--- github_secrets/provider.tf
+++ github_secrets/provider.tf
@@ -8,6 +8,6 @@ terraform {
 }
 
 provider "github" {
-  token = "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"
+  token = "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"
   owner = "agilis-ingenium"
 }
