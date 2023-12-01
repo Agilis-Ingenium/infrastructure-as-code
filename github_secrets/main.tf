4d90c8c07d7587bc4caf5e7051192d7734d06730
Warren Byron <20091892@mail.wit.ie>
2023-12-01 10:34:29 +0000

    Update main.tf

--- github_secrets/main.tf
+++ github_secrets/main.tf
@@ -1,14 +1,21 @@
 resource "github_actions_secret" "psql_url" {
   repository       = "database"
-  secret_name      = "psql_url"
+  secret_name      = "PSQL_URL"
   plaintext_value  = var.psql_url
 }
 
 # encrypted_value  = var.psql_url
 
 resource "github_actions_secret" "access_key_id" {
-  secret_name      = "HELLO"
+  secret_name      = "AWS_ACCESS_KEY_ID"
   plaintext_value  = var.access_key_id
+  for_each        = toset(var.sub_domains)
+  repository = "${each.value}.${var.domain_name}"
+}
+
+resource "github_actions_secret" "access_key_id" {
+  secret_name      = "AWS_SECRET_ACCESS_KEY"
+  plaintext_value  = var.access_key_secret
   for_each        = toset(var.sub_domains)
   repository = "${each.value}.${var.domain_name}"
 }
