2727f213cb904a4b73734d4229cc10cd3ccd1e90
Warren Byron <20091892@mail.wit.ie>
2023-12-01 09:53:53 +0000

    Update main.tf

--- iam/main.tf
+++ iam/main.tf
@@ -6,7 +6,7 @@ resource "aws_iam_user_policy" "s3_user_policy" {
   name = "s3_user_policy"
   user = aws_iam_user.s3_user.name
 policy = <<EOF
-  {
+{
     "Version": "2012-10-17",
     "Statement": [
       {
@@ -17,8 +17,8 @@ policy = <<EOF
         "Resource": "*"
       }
     ]
-  }
-  EOF
+}
+EOF
 }
 
 resource "aws_iam_access_key" "access_key" {
